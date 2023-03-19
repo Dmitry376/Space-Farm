c7ab2bd861891ab290a948754da724168dd4828d
Dmitry376 <106967455+Dmitry376@users.noreply.github.com>
2023-03-19 10:27:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import requests
 import sqlite3
 import streamlit as st
 import pandas as pd
+import json
 
-def main():
+
+def data_update():
     response = requests.get("https://dt.miet.ru/ppo_it_final",
                             headers={"X-Auth-Token": "xutgb523"})
-    print(response.text)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+
+
+
+def main():
+    st.set_page_config(
+        page_title="Дни",
+        layout="wide")
+
+    resbtn = st.button("Обновить данные")
+    if resbtn:
+        st.experimental_rerun()
+
+    st.title("Данные с датчиков температуры и влажности")
 
 main()
